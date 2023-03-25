709f1b394544011434ffa3ad1d8b3c1d8d3f3507
tikuwabux <badshinichi@gmail.com>
2023-03-25 11:21:34 +0900

    feat: サインアップ､サインイン､サインアウトのルーティングを追加する

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
   }
+
+  devise_scope :user do
+    get 'sign_up', :to => 'users/registrations#new'
+    get 'sign_in', :to => 'users/sessions#new'
+    get 'sign_out', :to => 'users/sessions#destroy'
+  end
 end
