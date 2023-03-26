7cc0dbf0932cf0a1692289c0dd54de61edbf80b1
tikuwabux <badshinichi@gmail.com>
2023-03-26 23:51:21 +0900

    feat: ホームページのパスをルートに変更する

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root to:'home#index'
+
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
