ab4e81c0b5b5e51ed66f423931ac1a6886124aa5
tikuwabux <badshinichi@gmail.com>
2023-03-31 11:26:15 +0900

    feat: #4 migrationを実行して､agenda_boardsテーブルを作成する

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_24_122606) do
+ActiveRecord::Schema.define(version: 2023_03_31_020906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "agenda_boards", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "agenda"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
