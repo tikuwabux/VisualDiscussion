2b997a22a7d623fe2accdf38818336589070181b
tikuwabux <badshinichi@gmail.com>
2023-03-31 19:54:04 +0900

    feat: #4 新規議題ボード作成機能を実装する

--- app/controllers/agenda_boards_controller.rb
+++ app/controllers/agenda_boards_controller.rb
@@ -1,2 +1,25 @@
 class AgendaBoardsController < ApplicationController
+  def new
+    @agenda_board = AgendaBoard.new
+  end
+
+  def create
+    @agenda_board = AgendaBoard.new(agenda_board_params)
+
+    binding.pry
+
+    if @agenda_board.save
+      flash[:notice] = "｢#{@agenda_board.agenda}｣のボード作成に成功しました"
+      redirect_to agenda_board_path(@agenda_board.id)
+    else
+      flash[:notice] = "｢#{@agenda_board.agenda}｣のボード作成に失敗しました"
+      render "new"
+    end
+  end
+
+  private
+
+  def agenda_board_params
+    params.require(:agenda_board).permit(:user_id, :agenda, :category)
+  end
 end
