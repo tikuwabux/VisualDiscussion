ac77638f20bcac5ec879df909cf7fc5cb7c11b83
tikuwabux <badshinichi@gmail.com>
2023-04-03 18:24:32 +0900

    test: #6 議題ボード一覧ページ周りのシステムスペックを追加する

--- spec/system/agenda_boards_spec.rb
+++ spec/system/agenda_boards_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "AgendaBoards", type: :system do
   let(:annie) { create(:user, name: "annie") }
+  let!(:about_early_bird) { create(:agenda_board, user_id: annie.id, agenda: "早起きは健康によいのか?", category: "自然科学") }
+  let!(:about_chatbot) { create(:agenda_board, user_id: annie.id, agenda: "チャットボットは教育に悪影響を与えるのか?", category: "社会科学") }
 
   describe "新規議題ボード作成ページアクセス後" do
     before do
@@ -27,6 +29,31 @@ RSpec.describe "AgendaBoards", type: :system do
       scenario "作成した議題ボードの詳細ページに遷移すること" do
         expect(page).to have_current_path agenda_board_path(annie.agenda_boards.last.id)
       end
+    end
+  end
+
+  describe "ログインユーザーが作成した議題ボード一覧ページにアクセス後" do
+    before do
+      visit root_path
+      click_on "ログイン"
+      fill_in "メールアドレス", with: annie.email
+      fill_in "パスワード", with: annie.password
+      click_button "Log in"
+      click_on "#{annie.name}さんが作成した議題ボード"
+    end
+
+    scenario "議題ボードの議題名の一覧を動的に確認できる" do
+      save_and_open_page
+      annie.agenda_boards.all? { |agenda_board| expect(page).to have_content agenda_board.agenda }
+    end
+
+    scenario "議題ボードのカテゴリ名の一覧を動的に確認できる" do
+      annie.agenda_boards.all? { |agenda_board| expect(page).to have_content agenda_board.category }
+    end
+
+    scenario "議題名をクリックすると､その議題ボードの詳細ページに遷移すること" do
+      click_on about_early_bird.agenda
+      expect(page).to have_current_path agenda_board_path(about_early_bird.id)
     end
   end
 end
