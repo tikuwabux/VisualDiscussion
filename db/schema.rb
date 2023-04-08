592381b5b7acab9606854d6bf2cc33eff3217a06
tikuwabux <badshinichi@gmail.com>
2023-04-08 13:54:27 +0900

    feat #8 migrationを実行して､evidencesテーブルを作成する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_08_043607) do
+ActiveRecord::Schema.define(version: 2023_04_08_044932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2023_04_08_043607) do
     t.integer "agenda_board_id"
     t.string "conclusion_summary"
     t.text "conclusion_detail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "evidences", force: :cascade do |t|
+    t.integer "reason_id"
+    t.string "evidence_summary"
+    t.text "evidence_detail"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
