1c06e1cf92a1833018983935a2854783469ab2d9
tikuwabux <badshinichi@gmail.com>
2023-04-08 13:27:42 +0900

    feat: #8 migrationを実行して､conclusionsテーブルを作成する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_31_020906) do
+ActiveRecord::Schema.define(version: 2023_04_08_042123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2023_03_31_020906) do
     t.integer "user_id"
     t.string "agenda"
     t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "conclusions", force: :cascade do |t|
+    t.integer "agenda_board_id"
+    t.string "conclusion_summary"
+    t.text "conclusion_detail"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
