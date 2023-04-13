35ea5286b3e3869d9cae91c6302bcd72bf09593f
tikuwabux <badshinichi@gmail.com>
2023-04-13 21:55:59 +0900

    test: #8 議題ボード詳細ページの主張の表示に関するシステムスペックを追加する

--- spec/system/agenda_boards_spec.rb
+++ spec/system/agenda_boards_spec.rb
@@ -2,8 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "AgendaBoards", type: :system do
   let(:annie) { create(:user, name: "annie") }
-  let!(:about_early_bird) { create(:agenda_board, user_id: annie.id, agenda: "早起きは健康によいのか?", category: "自然科学") }
+  let(:about_early_bird) { create(:agenda_board, user_id: annie.id, agenda: "早起きは健康によいのか?", category: "自然科学") }
   let!(:about_chatbot) { create(:agenda_board, user_id: annie.id, agenda: "チャットボットは教育に悪影響を与えるのか?", category: "社会科学") }
+  let(:bad_for_health) { create(:conclusion, agenda_board_id: about_early_bird.id, conclusion_summary: "健康に悪い") }
+  let!(:good_for_health) { create(:conclusion, agenda_board_id: about_early_bird.id, conclusion_summary: "健康に良い") }
+  let(:misalignment_with_body_clock) { create(:reason, conclusion_id: bad_for_health.id, reason_summary: "人間の体内時計と噛み合っていないから") }
+  let!(:lack_of_sleep) { create(:reason, conclusion_id: bad_for_health.id, reason_summary: "睡眠不足になりやすいから") }
+  let!(:sleep_data) { create(:evidence, reason_id: misalignment_with_body_clock.id, evidence_summary: "世界中のあらゆる人々の睡眠データ") }
+  let!(:research_of_dr_kelly) { create(:evidence, reason_id: misalignment_with_body_clock.id, evidence_summary: "ケリー博士の研究") }
 
   before do
     visit root_path
@@ -63,6 +69,23 @@ RSpec.describe "AgendaBoards", type: :system do
     scenario "｢新規主張作成｣ボタンをクリックすると､新規主張作成ページに遷移すること" do
       click_button "新規主張作成"
       expect(page).to have_current_path new_argument_path, ignore_query: true
+    end
+
+    scenario "すべての主張(結論+理由+証拠)を動的に確認できること" do
+      about_early_bird.conclusions.all? do |conclusion|
+        expect(page).to have_content conclusion.conclusion_summary
+        expect(page).to have_content conclusion.conclusion_detail
+
+        conclusion.reasons.all? do |reason|
+          expect(page).to have_content reason.reason_summary
+          expect(page).to have_content reason.reason_summary
+
+          reason.evidences.all? do |evidence|
+            expect(page).to have_content evidence.evidence_summary
+            expect(page).to have_content evidence.evidence_detail
+          end
+        end
+      end
     end
   end
 end
