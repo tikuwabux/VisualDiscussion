5d4e54de0f436b88cf7767312ca44a8e91384c29
tikuwabux <badshinichi@gmail.com>
2023-05-06 13:56:51 +0900

    feat: #11 主張､反論内にターゲットエンドポイントを設置する

--- app/javascript/packs/argument.js
+++ app/javascript/packs/argument.js
@@ -2,19 +2,21 @@ jsPlumb.ready(function() {
   jsPlumb.setContainer("exchange_of_opinions");
   // 主張をドラッグ可能にし､リロード後も主張の位置が再現されるようにする
   const all_arguments = document.querySelectorAll(".argument");
-
   all_arguments.forEach( function( argument ) {
     const argument_id = argument.getAttribute("id");
+
     // 主張をドラッグ可能にする
     jsPlumb.draggable(`${argument_id}`, {
       stop: function(event) {
         savePositions();
       }
     });
+
     // 主張のドラッグ終了後の位置をローカルストレージに保存する関数
     function savePositions() {
       localStorage.setItem(`${argument_id}_position`, JSON.stringify($(`#${argument_id}`).position()));
     }
+
     // ローカルストレージに保存された主張の位置を復元する
     const argument_position = JSON.parse(localStorage.getItem(`${argument_id}_position`));
     if (argument_position) {
@@ -24,7 +26,6 @@ jsPlumb.ready(function() {
 
   // 反論をドラッグ可能にし､リロード後も反論の位置が再現されるようにする
   const all_refutations = document.querySelectorAll(".refutation");
-
   all_refutations.forEach( function( refutation ) {
     const refutation_id = refutation.getAttribute("id");
 
@@ -45,15 +46,36 @@ jsPlumb.ready(function() {
   });
 
 
-  // 主張の表示がツリー型になるよう接続線を引く
-  const conclusions = document.querySelectorAll(".conclusion");
+  // 主張の表示がツリー型になるよう接続線を引く + 主張内にターゲットエンドポイントを設置する
+  jsPlumb.registerConnectionTypes({
+    "red-connection": {
+      paintStyle: {stroke: "red", strokeWidth: 5},
+      hoverPaintStyle: {stroke: "red", strokeWidth: 10}
+    }
+  });
 
+  const conclusions = document.querySelectorAll(".conclusion");
   conclusions.forEach( function( conclusion ) {
     const conclusion_id = conclusion.getAttribute("id");
-    const reasons_of_conclusion = document.querySelectorAll(`.reason_of_${conclusion_id}`);
 
+    // 結論の左右中央にターゲットエンドポイントを設置する
+    jsPlumb.addEndpoint(`${conclusion_id}`, {
+      endpoint: "Dot",
+      anchor: "RightMiddle",
+      isTarget: true,
+      connectionType: "red-connection"
+    })
+    jsPlumb.addEndpoint(`${conclusion_id}`, {
+      endpoint: "Dot",
+      anchor: "LeftMiddle",
+      isTarget: true,
+      connectionType: "red-connection"
+    })
+
+    const reasons_of_conclusion = document.querySelectorAll(`.reason_of_${conclusion_id}`);
     reasons_of_conclusion.forEach( function( reason ) {
       const reason_id = reason.getAttribute("id");
+
       // 結論 → endpoint 間に接続線を引く
       jsPlumb.connect({
         source: `${conclusion_id}`,
@@ -65,6 +87,15 @@ jsPlumb.ready(function() {
           ["Arrow", {width: 10, length: 10}]
         ]
       });
+
+      // endpointの真ん中にターゲットエンドポイントを設置する
+      jsPlumb.addEndpoint(`endpoint_between_${conclusion_id}_and_${reason_id}`, {
+        endpoint: "Dot",
+        anchor: "Center",
+        isTarget: true,
+        connectionType: "red-connection"
+      })
+
       // endpoint → 理由 間に接続線を引く
       jsPlumb.connect({
         source: `endpoint_between_${conclusion_id}_and_${reason_id}`,
@@ -77,8 +108,21 @@ jsPlumb.ready(function() {
         ]
       });
 
-      const evidences_of_reason = document.querySelectorAll(`.evidence_of_${reason_id}`);
+      // 理由の左右中央にターゲットエンドポイントを設置する
+      jsPlumb.addEndpoint(`${reason_id}`, {
+        endpoint: "Dot",
+        anchor: "RightMiddle",
+        isTarget: true,
+        connectionType: "red-connection"
+      })
+      jsPlumb.addEndpoint(`${reason_id}`, {
+        endpoint: "Dot",
+        anchor: "LeftMiddle",
+        isTarget: true,
+        connectionType: "red-connection"
+      })
 
+      const evidences_of_reason = document.querySelectorAll(`.evidence_of_${reason_id}`);
       evidences_of_reason.forEach( function( evidence ) {
         const evidence_id = evidence.getAttribute("id");
         // 理由 → endpoint 間に接続線を引く
@@ -92,6 +136,15 @@ jsPlumb.ready(function() {
             ["Arrow", {width: 10, length: 10}]
           ]
         });
+
+        // endpointの真ん中にターゲットエンドポイントを設置する
+        jsPlumb.addEndpoint(`endpoint_between_${reason_id}_and_${evidence_id}`, {
+          endpoint: "Dot",
+          anchor: "Center",
+          isTarget: true,
+          connectionType: "red-connection"
+        })
+
         // endpoint → 証拠 間に接続線を引く
         jsPlumb.connect({
           source: `endpoint_between_${reason_id}_and_${evidence_id}`,
@@ -103,24 +156,43 @@ jsPlumb.ready(function() {
             ["Arrow", {width: 10, length: 10}]
           ]
         });
+
+        // 証拠の左右中央にターゲットエンドポイントを設置する
+        jsPlumb.addEndpoint(`${evidence_id}`, {
+          endpoint: "Dot",
+          anchor: "RightMiddle",
+          isTarget: true,
+          connectionType: "red-connection"
+        })
+        jsPlumb.addEndpoint(`${evidence_id}`, {
+          endpoint: "Dot",
+          anchor: "LeftMiddle",
+          isTarget: true,
+          connectionType: "red-connection"
+        })
       });
     });
   });
 
-  // 反論(サンプル)の表示がツリー型になるよう接続線を引く
+  // 反論(サンプル)の表示がツリー型になるよう接続線を引く + 反論内にターゲットエンドポイントを設置する
   const ref_conclusions = document.querySelectorAll(".ref_conclusion");
-
-  jsPlumb.registerConnectionTypes({
-    "red-connection": {
-      paintStyle: {stroke: "red", strokeWidth: 5},
-      hoverPaintStyle: {stroke: "red", strokeWidth: 10}
-    }
-  });
-
   ref_conclusions.forEach( function( ref_conclusion ) {
     const ref_conclusion_id = ref_conclusion.getAttribute("id");
-    const ref_reasons_of_ref_conclusion = document.querySelectorAll(`.ref_reason_of_${ref_conclusion_id}`);
 
+    jsPlumb.addEndpoint(`${ref_conclusion_id}`, {
+      endpoint: "Dot",
+      anchor: "RightMiddle",
+      isTarget: true,
+      connectionType: "red-connection"
+    })
+    jsPlumb.addEndpoint(`${ref_conclusion_id}`, {
+      endpoint: "Dot",
+      anchor: "LeftMiddle",
+      isTarget: true,
+      connectionType: "red-connection"
+    })
+
+    const ref_reasons_of_ref_conclusion = document.querySelectorAll(`.ref_reason_of_${ref_conclusion_id}`);
     ref_reasons_of_ref_conclusion.forEach( function( ref_reason ) {
       const ref_reason_id = ref_reason.getAttribute("id");
 
@@ -135,6 +207,13 @@ jsPlumb.ready(function() {
         ]
       });
 
+      jsPlumb.addEndpoint(`endpoint_between_${ref_conclusion_id}_and_${ref_reason_id}`, {
+        endpoint: "Dot",
+        anchor: "Center",
+        isTarget: true,
+        connectionType: "red-connection",
+      })
+
       jsPlumb.connect({
         source: `endpoint_between_${ref_conclusion_id}_and_${ref_reason_id}`,
         target: `${ref_reason_id}`,
@@ -145,6 +224,19 @@ jsPlumb.ready(function() {
           ["Arrow", {width: 10, length: 10}]
         ]
       });
+
+      jsPlumb.addEndpoint(`${ref_reason_id}`, {
+        endpoint: "Dot",
+        anchor: "RightMiddle",
+        isTarget: true,
+        connectionType: "red-connection"
+      })
+      jsPlumb.addEndpoint(`${ref_reason_id}`, {
+        endpoint: "Dot",
+        anchor: "LeftMiddle",
+        isTarget: true,
+        connectionType: "red-connection"
+      })
 
       const ref_evidences_of_ref_reason = document.querySelectorAll(`.ref_evidence_of_${ref_reason_id}`);
 
@@ -162,6 +254,13 @@ jsPlumb.ready(function() {
           ]
         });
 
+        jsPlumb.addEndpoint(`endpoint_between_${ref_reason_id}_and_${ref_evidence_id}`, {
+          endpoint: "Dot",
+          anchor: "Center",
+          isTarget: true,
+          connectionType: "red-connection"
+        })
+
         jsPlumb.connect({
           source: `endpoint_between_${ref_reason_id}_and_${ref_evidence_id}`,
           target: `${ref_evidence_id}`,
@@ -172,6 +271,19 @@ jsPlumb.ready(function() {
             ["Arrow", {width: 10, length: 10}]
           ]
         });
+
+        jsPlumb.addEndpoint(`${ref_evidence_id}`, {
+          endpoint: "Dot",
+          anchor: "RightMiddle",
+          isTarget: true,
+          connectionType: "red-connection"
+        })
+        jsPlumb.addEndpoint(`${ref_evidence_id}`, {
+          endpoint: "Dot",
+          anchor: "LeftMiddle",
+          isTarget: true,
+          connectionType: "red-connection"
+        })
       });
     });
   });
