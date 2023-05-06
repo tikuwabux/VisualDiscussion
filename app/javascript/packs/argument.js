8eaeee043baee5afc245e6d9c31dedef4e040b6e
tikuwabux <badshinichi@gmail.com>
2023-05-06 11:52:49 +0900

    feat: #11 反論のサンプルを2つ追加する

--- app/javascript/packs/argument.js
+++ app/javascript/packs/argument.js
@@ -1,5 +1,5 @@
-// 主張の表示がツリー型になるよう線を引く
 jsPlumb.ready(function() {
+  // 主張の表示がツリー型になるよう接続線を引く
   const conclusions = document.querySelectorAll(".conclusion");
 
   conclusions.forEach( function( conclusion ) {
@@ -8,7 +8,7 @@ jsPlumb.ready(function() {
 
     reasons_of_conclusion.forEach( function( reason ) {
       const reason_id = reason.getAttribute("id");
-      // 結論 → endponit
+      // 結論 → endpoint 間に接続線を引く
       jsPlumb.connect({
         source: `${conclusion_id}`,
         target: `endpoint_between_${conclusion_id}_and_${reason_id}`,
@@ -19,7 +19,7 @@ jsPlumb.ready(function() {
           ["Arrow", {width: 10, length: 10}]
         ]
       });
-      // endponit → 理由
+      // endpoint → 理由 間に接続線を引く
       jsPlumb.connect({
         source: `endpoint_between_${conclusion_id}_and_${reason_id}`,
         target: `${reason_id}`,
@@ -35,7 +35,7 @@ jsPlumb.ready(function() {
 
       evidences_of_reason.forEach( function( evidence ) {
         const evidence_id = evidence.getAttribute("id");
-        // 理由 → endpoint
+        // 理由 → endpoint 間に接続線を引く
         jsPlumb.connect({
           source: `${reason_id}`,
           target: `endpoint_between_${reason_id}_and_${evidence_id}`,
@@ -46,10 +46,79 @@ jsPlumb.ready(function() {
             ["Arrow", {width: 10, length: 10}]
           ]
         });
-        // endpoint → 証拠
+        // endpoint → 証拠 間に接続線を引く
         jsPlumb.connect({
           source: `endpoint_between_${reason_id}_and_${evidence_id}`,
           target: `${evidence_id}`,
+          anchors: ["Bottom", "Top"],
+          connector: "Straight",
+          endpoint:"Blank",
+          overlays:[
+            ["Arrow", {width: 10, length: 10}]
+          ]
+        });
+      });
+    });
+  });
+
+  // 反論(サンプル)の表示がツリー型になるよう接続線を引く
+  const ref_conclusions = document.querySelectorAll(".ref_conclusion");
+
+  jsPlumb.registerConnectionTypes({
+    "red-connection": {
+      paintStyle: {stroke: "red", strokeWidth: 5},
+      hoverPaintStyle: {stroke: "red", strokeWidth: 10}
+    }
+  });
+
+  ref_conclusions.forEach( function( ref_conclusion ) {
+    const ref_conclusion_id = ref_conclusion.getAttribute("id");
+    const ref_reasons_of_ref_conclusion = document.querySelectorAll(`.ref_reason_of_${ref_conclusion_id}`);
+
+    ref_reasons_of_ref_conclusion.forEach( function( ref_reason ) {
+      const ref_reason_id = ref_reason.getAttribute("id");
+
+      jsPlumb.connect({
+        source: `${ref_conclusion_id}`,
+        target: `endpoint_between_${ref_conclusion_id}_and_${ref_reason_id}`,
+        anchors: ["Bottom", "Top"],
+        connector: "Straight",
+        endpoint:"Blank",
+        overlays:[
+          ["Arrow", {width: 10, length: 10}]
+        ]
+      });
+
+      jsPlumb.connect({
+        source: `endpoint_between_${ref_conclusion_id}_and_${ref_reason_id}`,
+        target: `${ref_reason_id}`,
+        anchors: ["Bottom", "Top"],
+        connector: "Straight",
+        endpoint:"Blank",
+        overlays:[
+          ["Arrow", {width: 10, length: 10}]
+        ]
+      });
+
+      const ref_evidences_of_ref_reason = document.querySelectorAll(`.ref_evidence_of_${ref_reason_id}`);
+
+      ref_evidences_of_ref_reason.forEach( function( ref_evidence ) {
+        const ref_evidence_id = ref_evidence.getAttribute("id");
+
+        jsPlumb.connect({
+          source: `${ref_reason_id}`,
+          target: `endpoint_between_${ref_reason_id}_and_${ref_evidence_id}`,
+          anchors: ["Bottom", "Top"],
+          connector: "Straight",
+          endpoint:"Blank",
+          overlays:[
+            ["Arrow", {width: 10, length: 10}]
+          ]
+        });
+
+        jsPlumb.connect({
+          source: `endpoint_between_${ref_reason_id}_and_${ref_evidence_id}`,
+          target: `${ref_evidence_id}`,
           anchors: ["Bottom", "Top"],
           connector: "Straight",
           endpoint:"Blank",
