30a87d49665aba6c5e68cb970f59388151627523
tikuwabux <badshinichi@gmail.com>
2023-05-19 20:03:04 +0900

    refactor: #15 「Google JavaScript Style Guide｣に従い､jsの変数名をキャメルケースにする

--- app/javascript/packs/create_new_argument.js
+++ app/javascript/packs/create_new_argument.js
@@ -1,34 +1,34 @@
 document.addEventListener('input', function(event) {
   const element = event.target;
-  const computed_element_style = window.getComputedStyle(element);
-  const entered_text = element.value;
-  const font_in_computed_element_style = computed_element_style.getPropertyValue('font');
-  const empty_canvas_element = document.createElement('canvas');
-  const blank_2d_context = empty_canvas_element.getContext('2d');
-  blank_2d_context.font = font_in_computed_element_style;
+  const computedElementStyle = window.getComputedStyle(element);
+  const enteredText = element.value;
+  const fontInComputedElementStyle = computedElementStyle.getPropertyValue('font');
+  const emptyCanvasElement = document.createElement('canvas');
+  const blank2dContext = emptyCanvasElement.getContext('2d');
+  blank2dContext.font = fontInComputedElementStyle;
 
   // 改行不可能なフォームの場合､その幅を､入力した文字列の幅､に合わせて伸縮させる
   if (element.matches('.adjust-entered-text-width')) {
-    const entered_text_width = blank_2d_context.measureText(entered_text).width;
-    const element_side_padding = parseInt(computed_element_style.getPropertyValue('padding-left')) + parseInt(computed_element_style.getPropertyValue('padding-right'));
-    const adjusted_entered_text_width = entered_text_width + element_side_padding;
-    element.style.width = adjusted_entered_text_width + 'px';
+    const enteredTextWidth = blank2dContext.measureText(enteredText).width;
+    const elementSidePadding = parseInt(computedElementStyle.getPropertyValue('padding-left')) + parseInt(computedElementStyle.getPropertyValue('padding-right'));
+    const adjustedEnteredTextWidth = enteredTextWidth + elementSidePadding;
+    element.style.width = adjustedEnteredTextWidth + 'px';
   }
 
   // 改行可能なフォームの場合､その幅を､最大幅の文字列を含む行の幅に､その高さを全行の高さに合わせて伸縮させる｡
   // (3行目以降､最終行に謎の空行が出現する不具合あり)
   if (element.matches('.adjust-entered-text-size')) {
-    const lines = entered_text.split('\n');
-    let max_line_width = 0;
+    const lines = enteredText.split('\n');
+    let maxLineWidth = 0;
     lines.forEach(function(line) {
-      const line_width = blank_2d_context.measureText(line).width;
-      if (line_width > max_line_width) {
-        max_line_width = line_width;
+      const lineWidth = blank2dContext.measureText(line).width;
+      if (lineWidth > maxLineWidth) {
+        maxLineWidth = lineWidth;
       }
     });
-    const element_side_padding = parseInt(computed_element_style.getPropertyValue('padding-left')) + parseInt(computed_element_style.getPropertyValue('padding-right'));
-    const adjusted_entered_text_width = max_line_width + element_side_padding;
-    element.style.width = adjusted_entered_text_width + 'px';
+    const elementSidePadding = parseInt(computedElementStyle.getPropertyValue('padding-left')) + parseInt(computedElementStyle.getPropertyValue('padding-right'));
+    const adjustedEnteredTextWidth = maxLineWidth + elementSidePadding;
+    element.style.width = adjustedEnteredTextWidth + 'px';
     element.style.height = "auto"; // この一行が無いと､文字を入力するたびに､入力があったtextareaの高さが不要に伸びる
     element.style.height = `${element.scrollHeight}px`;
   }
