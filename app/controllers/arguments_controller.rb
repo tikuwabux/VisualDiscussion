101b3f105999554ab9923568741f3f44747eb778
tikuwabux <badshinichi@gmail.com>
2023-05-24 17:20:41 +0900

    feat #15 主張の編集機能を実装する

--- app/controllers/arguments_controller.rb
+++ app/controllers/arguments_controller.rb
@@ -20,6 +20,27 @@ class ArgumentsController < ApplicationController
     end
   end
 
+  def edit
+    @agenda_board_agenda = params[:agenda_board_agenda]
+    conclusion_id = params[:edit_target_conclusion_id].to_i
+    @conclusion = Conclusion.find(conclusion_id)
+  end
+
+  # 主張編集ページでの編集中や､編集完了後の議題ボード詳細ページでの表示において､
+  # 理由BOXや証拠BOXがupdate_atカラム値基準の昇順で左から右に並んでしまう不具合あり｡
+  # => 反論からの接続線がついてるBOXを編集すると､編集完了後の表示において､編集したBOXが右端に移動してしまい､元いた位置に移動した別のBOXに接続線がつけ変わったりするため､致命的な不具合｡
+  def update
+    argument_id = params[:edit_target_conclusion_id].to_i
+    argument = Conclusion.find(argument_id)
+    if argument.update(argument_params)
+      flash[:notice] = "主張の編集に成功しました"
+      redirect_to agenda_board_path(argument.agenda_board_id)
+    else
+      flash[:notice] = "主張の編集に失敗しました"
+      render "edit"
+    end
+  end
+
   private
 
   def argument_params
