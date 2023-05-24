7bd4a3e7902e2f4fc621f53b7be2d7df391a944d
tikuwabux <badshinichi@gmail.com>
2023-05-24 17:51:20 +0900

    fix: #15 編集中の主張のBOX表示や､編集完了後の主張のBOX表示の並び順を統一する｡
    ｢created_atカラム値基準の昇順で､左から右並び｣に統一にする

--- app/controllers/arguments_controller.rb
+++ app/controllers/arguments_controller.rb
@@ -26,9 +26,6 @@ class ArgumentsController < ApplicationController
     @conclusion = Conclusion.find(conclusion_id)
   end
 
-  # 主張編集ページでの編集中や､編集完了後の議題ボード詳細ページでの表示において､
-  # 理由BOXや証拠BOXがupdate_atカラム値基準の昇順で左から右に並んでしまう不具合あり｡
-  # => 反論からの接続線がついてるBOXを編集すると､編集完了後の表示において､編集したBOXが右端に移動してしまい､元いた位置に移動した別のBOXに接続線がつけ変わったりするため､致命的な不具合｡
   def update
     argument_id = params[:edit_target_conclusion_id].to_i
     argument = Conclusion.find(argument_id)
