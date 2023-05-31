e2c57e88ab09932bde3e09a90f7aa536be5dff65
tikuwabux <badshinichi@gmail.com>
2023-05-31 16:20:23 +0900

    refactor: #15 反論編集ページのpath中に､編集対象のref_conclusionのid値を付加し､余剰なパラメーターの受け渡しを解消する

--- app/controllers/refutations_controller.rb
+++ app/controllers/refutations_controller.rb
@@ -28,9 +28,8 @@ class RefutationsController < ApplicationController
   end
 
   def edit
-    @agenda_board_agenda = params[:agenda_board_agenda]
-    ref_conclusion_id = params[:edit_target_ref_conclusion_id].to_i
-    @ref_conclusion = RefConclusion.find(ref_conclusion_id)
+    @ref_conclusion = RefConclusion.find(params[:id])
+    @agenda_board_agenda = AgendaBoard.find(@ref_conclusion.agenda_board_id).agenda
 
     if @ref_conclusion.conclusion_id
       @rebuttal_target_conclusion = Conclusion.find(@ref_conclusion.conclusion_id)
@@ -40,8 +39,8 @@ class RefutationsController < ApplicationController
   end
 
   def update
-    refutation_id = params[:edit_target_ref_conclusion_id].to_i
-    refutation = RefConclusion.find(refutation_id)
+    refutation = RefConclusion.find(params[:id])
+
     if refutation.update(refutation_params)
       flash[:notice] = "反論の編集に成功しました"
       redirect_to agenda_board_path(refutation.agenda_board_id)
