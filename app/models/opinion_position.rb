915f90ea0ba52ed8beed8847e88b2e9c7af3e6fb
tikuwabux <badshinichi@gmail.com>
2023-06-14 02:03:40 +0900

    feat: #19 RefConclusionモデルy:OpinionPositionモデル = 1 : 1 で関連付ける

--- app/models/opinion_position.rb
+++ app/models/opinion_position.rb
@@ -1,3 +1,4 @@
 class OpinionPosition < ApplicationRecord
   belongs_to :argument, class_name: 'Conclusion', optional: true
+  belongs_to :refutation, class_name: 'RefConclusion', optional: true
 end
