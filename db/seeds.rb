89764cc07f71ad8c9f4cfe6e13a622493e4c30e9
tikuwabux <badshinichi@gmail.com>
2023-06-30 14:40:37 +0900

    feat: #19 意見間の接続線情報のサンプルデータをseedファイルに追記する

--- db/seeds.rb
+++ db/seeds.rb
@@ -166,3 +166,16 @@ OpinionPosition.create!(
     }
   ]
 )
+
+OpinionConnection.create!(
+  [
+    {
+      source_id: 'refutation1',
+      target_id: 'reason0_of_conclusion1'
+    },
+    {
+      source_id: 'refutation2',
+      target_id: 'endpoint_between_ref_reason0_of_ref_conclusion1_and_ref_evidence0_of_ref_reason0_of_ref_conclusion1'
+    }
+  ]
+)
