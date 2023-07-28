cfc4c129e8ea50f8841c23d731545bf2e6fcb320
tikuwabux <badshinichi@gmail.com>
2023-07-28 21:24:57 +0900

    feat #30 agenda_boardモデルのバリデーションとそれに対応するテストを追加する

--- app/models/agenda_board.rb
+++ app/models/agenda_board.rb
@@ -2,4 +2,6 @@ class AgendaBoard < ApplicationRecord
   belongs_to :user
   has_many :conclusions
   has_many :ref_conclusions
+
+  validates :agenda, presence: true, uniqueness: true
 end
--- spec/models/agenda_board_spec.rb
+++ spec/models/agenda_board_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe AgendaBoard, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:annie) { create(:user, name: "annie") }
+  let!(:about_early_bird) { create(:agenda_board, user_id: annie.id, agenda: "早起きは健康によいのか?", category: "自然科学") }
+  let!(:dummy_agenda_board) { create(:agenda_board, user_id: annie.id) }
+
+  it "適切な｢議題｣､｢カテゴリ｣､｢作成者したuserのid値｣があれば有効な状態であること" do
+    expect(about_early_bird).to be_valid
+  end
+
+  it "議題が無ければ無効な状態であること" do
+    dummy_agenda_board.agenda = nil
+    dummy_agenda_board.valid?
+    expect(dummy_agenda_board.errors.full_messages.first).to eq "議題を入力してください"
+  end
+
+  it "重複した議題なら無効な状態であること" do
+    dummy_agenda_board.agenda = "早起きは健康によいのか?"
+    dummy_agenda_board.valid?
+    expect(dummy_agenda_board.errors.full_messages.first).to eq "議題はすでに存在します"
+  end
 end
