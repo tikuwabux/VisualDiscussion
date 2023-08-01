f119108a674bb469d91cafaa29b446f0bc919971
tikuwabux <badshinichi@gmail.com>
2023-08-01 13:46:11 +0900

    test #30 conclusion, reason, evidenceのモデルスペックを追加する

--- spec/models/conclusion_spec.rb
+++ spec/models/conclusion_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Conclusion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:annie) { create(:user, name: "annie") }
+  let(:about_early_bird) { create(:agenda_board, user_id: annie.id, agenda: "早起きは健康によいのか?", category: "自然科学") }
+  let!(:bad_for_health) { create(:conclusion, conclusion_summary: "健康に悪い", conclusion_detail: nil, agenda_board_id: about_early_bird.id, user_id: annie.id) }
+
+  it "有効な｢結論｣,｢所属する議題ボードのid値｣,｢作成したユーザーのid値｣があれば有効な状態であること" do
+    expect(bad_for_health).to be_valid
+  end
+
+  it "結論が無ければ無効な状態であること" do
+    bad_for_health.conclusion_summary = nil
+    bad_for_health.valid?
+    expect(bad_for_health.errors.full_messages.first).to eq "結論を入力してください"
+  end
+
+  it "所属する議題ボードのid値が無ければ無効な状態であること" do
+    bad_for_health.agenda_board_id = nil
+    bad_for_health.valid?
+    expect(bad_for_health.errors.full_messages.first).to eq "所属する議題ボードのid値を入力してください"
+  end
+
+  it "作成者であるユーザーのid値が無ければ無効な状態であること" do
+    bad_for_health.user_id = nil
+    bad_for_health.valid?
+    expect(bad_for_health.errors.full_messages.first).to eq "作成者であるユーザーのid値を入力してください"
+  end
 end
--- spec/models/reason_spec.rb
+++ spec/models/reason_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Reason, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:annie) { create(:user, name: "annie") }
+  let(:about_early_bird) { create(:agenda_board, user_id: annie.id, agenda: "早起きは健康によいのか?", category: "自然科学") }
+  let(:bad_for_health) { create(:conclusion, conclusion_summary: "健康に悪い", conclusion_detail: nil, agenda_board_id: about_early_bird.id, user_id: annie.id) }
+  let!(:misalignment_with_body_clock) { create(:reason, reason_summary: "人間の体内時計と噛み合っていないから", reason_detail: nil, conclusion_id: bad_for_health.id) }
+
+  it "有効な｢理由｣,｢帰属先である結論のid値｣があれば有効な状態であること" do
+    expect(misalignment_with_body_clock).to be_valid
+  end
+
+  it "理由が無ければ無効な状態であること" do
+    misalignment_with_body_clock.reason_summary = nil
+    misalignment_with_body_clock.valid?
+    expect(misalignment_with_body_clock.errors.full_messages.first).to eq "理由を入力してください"
+  end
+
+  it "帰属先である結論のid値が無ければ無効な状態であること" do
+    misalignment_with_body_clock.conclusion_id = nil
+    misalignment_with_body_clock.valid?
+    expect(misalignment_with_body_clock.errors.full_messages.first).to eq "帰属先である結論のid値を入力してください"
+  end
 end
