0e3112e613c5f4b9cc0427e58ba98535d7141236
tikuwabux <badshinichi@gmail.com>
2023-08-10 19:08:05 +0900

    feat #31 主張の編集/削除は作成者のみが実行できるよう､アクセス制限を課す

--- spec/requests/arguments_spec.rb
+++ spec/requests/arguments_spec.rb
@@ -14,8 +14,20 @@ RSpec.shared_examples("未ログインユーザーに対するアクセス制限
   end
 end
 
+RSpec.shared_examples("主張の編集/削除に関するアクセス制限のテスト") do
+  it "302リダイレクトを返すこと" do
+    expect(response).to have_http_status(302)
+  end
+
+  it "設定したフラッシュメッセージがセットされていること" do
+    expect(flash[:alert]).to eq("編集/削除する権限があるのは､あなた自身が作成した主張のみです")
+  end
+end
+
 RSpec.describe "Arguments", type: :request do
   let(:annie) { create(:user, name: "annie") }
+  let(:brian) { create(:user, name: "brian") }
+
   let(:about_early_bird) { create(:agenda_board, user_id: annie.id, agenda: "早起きは健康によいのか?", category: "自然科学") }
 
   let(:very_bad_for_health) do
@@ -52,6 +64,39 @@ RSpec.describe "Arguments", type: :request do
     describe "DELETE /arguments/:id" do
       before { delete argument_path(very_bad_for_health.id) }
       include_examples("未ログインユーザーに対するアクセス制限のテスト")
+    end
+  end
+
+  context "既ログイン時" do
+    before { sign_in(brian) }
+
+    describe "以下の権限のない主張の編集/削除に関するリクエストを行った時" do
+      describe "GET /arguments/:id/edit" do
+        before { get edit_argument_path(very_bad_for_health.id) }
+
+        include_examples("主張の編集/削除に関するアクセス制限のテスト")
+        it "編集しようとした主張が所属する議題ボード詳細ページへリダイレクトされること" do
+          expect(response).to redirect_to(agenda_board_path(very_bad_for_health.agenda_board_id))
+        end
+      end
+
+      describe "PATCH /arguments/:id" do
+        before { patch argument_path(very_bad_for_health.id) }
+
+        include_examples("主張の編集/削除に関するアクセス制限のテスト")
+        it "編集しようとした主張が所属する議題ボード詳細ページへリダイレクトされること" do
+          expect(response).to redirect_to(agenda_board_path(very_bad_for_health.agenda_board_id))
+        end
+      end
+
+      describe "DELETE /arguments/:id" do
+        before { delete argument_path(very_bad_for_health.id) }
+
+        include_examples("主張の編集/削除に関するアクセス制限のテスト")
+        it "削除しようとした主張が所属する議題ボード詳細ページへリダイレクトされること" do
+          expect(response).to redirect_to(agenda_board_path(very_bad_for_health.agenda_board_id))
+        end
+      end
     end
   end
 end
